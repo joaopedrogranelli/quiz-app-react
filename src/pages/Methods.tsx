941218fd9ee156e44bd495c4043630469ff2d901
João Pedro Granelli <joao.granelli123@gmail.com>
2025-02-13 17:32:14 -0300

    feat: Estilazação da página de Metodos

--- src/pages/Methods.tsx
+++ src/pages/Methods.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import styles from '../styles/methods.module.scss';
 
 const Methods: React.FC = () => {
   const navigate = useNavigate();
@@ -9,15 +11,48 @@ const Methods: React.FC = () => {
   };
 
   return (
-    <div className="methods-container">
-      <h1>🏅 Métodos de Estudo 🏅</h1>
-      <div className="buttons">
-        {[30, 50, 100].map((qtd) => (
-          <button key={qtd} onClick={() => handleMethod(qtd)}>
-            {qtd} Questões
-          </button>
-        ))}
-      </div>
+    <div className={styles.methodsContainer}>
+      <header className={styles.methodsHeader}>
+        <button className={styles.backButton} onClick={() => navigate('/')}>⟵</button>
+        <h1>🏅 Métodos 🏅</h1>
+      </header>
+
+      <main className={styles.methodsMain}>
+        <div className={styles.methodsBox}>
+          <h2>Selecione um método de estudo:</h2>
+          <h5>Escolha quantas questões quer estudar por dia</h5>
+          <div className={styles.buttons}>
+            {[30, 50, 100].map((qtd) => (
+              <button
+                key={qtd}
+                className={styles.methodButton}
+                onClick={() => handleMethod(qtd)}
+              >
+                {qtd} Questões
+              </button>
+            ))}
+          </div>
+        </div>
+
+        <div className={styles.methodsBox}>
+          <h2>Personalize seu método de estudo:</h2>
+          <h5>Digite quantas questões quer estudar por dia</h5>
+          <div className={styles.customMethod}>
+            <input
+              type="number"
+              placeholder="🥳 Você consegue! 🥳"
+              min="1"
+              max="100"
+              className={styles.customInput}
+            />
+            <button className={styles.customButton}>Escolher</button>
+          </div>
+        </div>
+      </main>
+
+      <footer className={styles.methodsFooter}>
+        <p>Matheus Meissner | João Pedro Granelli</p>
+      </footer>
     </div>
   );
 };
